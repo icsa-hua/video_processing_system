4684ba1c77b1248459ef10ad7eb6ae3a95bee40f
Dimitrios Georgiadis <jimgeorgiadis7@gmail.com>
2026-05-29 17:48:16 +0300

    fix(logger): stdout habdler is now always added

--- obs_system/utils/logger.py
+++ obs_system/utils/logger.py
@@ -56,7 +56,7 @@ def setup_logging(level=logging.INFO, log_dir="assets/logs"):
 
     jupyter_handler = jupyter_logger(level=level)
 
-    if not any(isinstance(h, logging.StreamHandler) for h in root.handlers):
+    if not any(isinstance(h, logging.StreamHandler) and not isinstance(h, logging.FileHandler) for h in root.handlers):
         root.addHandler(jupyter_handler) 
 
     root.propagate = True
